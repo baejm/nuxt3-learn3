098e06fb0ec09246dc2131637e492a75925eab88
gymcoding <bruce.lean17@gmail.com>
2024-01-11 10:42:15 +0900

    31 Auth Named Middleware 적용

---
+++ middleware/guest-only.ts
@@ -0,0 +1,7 @@
+export default defineNuxtRouteMiddleware(() => {
+  const { isAuthenticated } = useAuthUser();
+  if (isAuthenticated.value) {
+    if (process.server) return navigateTo('/');
+    return abortNavigation();
+  }
+});
